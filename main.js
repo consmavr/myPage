ac821fd02ac6932980cb0362602d224395ba61e9
consmavr <consmavr@gmail.com>
2015-04-07 21:23:47 +0300

    whatever

--- main.js
+++ main.js
@@ -2,19 +2,66 @@
 
 $(function(){
   var $orders = $('#orders');
+  var $name = $('#name');
+  var $drink = $('#drink');
+
+  var orderTemplate = ""+
+  "<li>"+
+  "<p><strong>Name:</strong> {{name}}</p>"+
+  "<p><strong> Drink:</strong> {{drink}}</p>"+
+  "<button data-id='{{id}}' class='remove'>X</button>"+
+  "</li>";
+
+  function addOrder(order){
+    $orders.append(Mustache.render(orderTemplate, order));
+
+  }
 
   $.ajax({
     type: 'GET',
-    url: 'api/orders',
+    url: 'http://rest.learncode.academy/api/johnbob/friends',
     success: function(orders){
       $.each(orders, function(i, order){
-        $orders.append('<li>name: '+ order.name +', drink: '+ order.drink+ '</li>');
+        addOrder(order);
       });
+    },
+    error: function(){
+      alert('error loading orders');
     }
 
   });
 
+  $('#add-order').on('click', function(){
+      var order = {
+        name: $name.val(),
+        drink: $drink.val(),
+      };
 
+      $.ajax({
+        type: 'POST',
+        url: 'http://rest.learncode.academy/api/johnbob/friends',
+        data: order,
+        success: function(newOrder){
+          addOrder(newOrder);
+        },
+        error: function(){
+          alert('error saving order');
+        }
+      });
+
+  });
+
+  $orders.delegate('.remove', 'click',function(){
+    var $li = $(this).closest('li');
+
+    $.ajax({
+      type: 'DELETE',
+      url: 'http://rest.learncode.academy/api/johnbob/friends' + $(this).attr('data-id'),
+      success: function(){
+        $li.remove();
+      }
+    });
+  });
 });
 
 /* $(function(){
