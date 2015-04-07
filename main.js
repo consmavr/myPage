f06095d36132e10d429d40016b0a79fa1b2da677
consmavr <consmavr@gmail.com>
2015-04-07 19:47:30 +0300

    ajax stuff

---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+$(function(){
+  var $orders = $('#orders');
+
+  $.ajax({
+    type: 'GET',
+    url: 'api/orders',
+    success: function(orders){
+      $.each(orders, function(i, order){
+        $orders.append('<li>name: '+ order.name +', drink: '+ order.drink+ '</li>');
+      });
+    }
+
+  });
+
+
+});
+
+/* $(function(){
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find('.slides');
+    var $slides = $slideContainer.find(".slide");
+
+    var interval;
+
+    function startSlider(){
+        interval = setInterval(function(){
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed,function(){
+                currentSlide++;
+                if(currentSlide == $slides.length){
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+
+
+        }, pause);
+    }
+
+    function stopSlider(){
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+    //startSlider();
+}); */
